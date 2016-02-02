477eb75273f690208adad4837477cc22d64f982d
Mike Labita <mlabita37@gmail.com>
2016-02-01 22:16:52 -0500

    Adds mongoDB and mongoose module

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 var express     =   require('express');
 var morgan      =   require('morgan');
 var bodyParser  =   require('body-parser');
+var mongoose    =   require('mongoose');
 
 // App Build and Config
 var app = express();
 app.use(express.static('./client/public'));
+
 app.use( bodyParser.urlencoded({extended: true}) );
+
+var mongoPath = 'mongodb://localhost/passenger-01';
+mongoose.connect(mongoPath);
 
 app.set('views', __dirname + '/client/views')
 app.set('view engine', 'ejs')
